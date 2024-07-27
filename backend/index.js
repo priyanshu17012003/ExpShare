68d73b66a2a7c27ea26d11977cf8ab86a1ec975e
priyanshu17012003 <priyanshusanki123@gmail.com>
2024-07-27 19:00:00 +0530

    Fourth Commit

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const mongoose=require("mongoose");
 const express=require("express");
 const cors=require("cors");
+const path=require("path");
 const cookieParser=require("cookie-parser");
 const routeList=require("./routes/list-route")
 const routeUser=require('./routes/user-route');
@@ -39,6 +40,15 @@ app.use("/api/profile",routeProfile);
 app.use("/api/message",routeMessage);
 app.use("/api/conversation",routeMembers);
 
+//-----------------------code for deployment------------------------------------
+if(process.env.NODE_ENV==='production'){
+    const dirPath=path.resolve();
+
+    app.use(express.static('./frontend/dist'));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(dirPath,'./frontend/dist','index.html'));
+    })
+}
 
 server.listen(port,()=>(
     console.log(`Running on ${port}`)
